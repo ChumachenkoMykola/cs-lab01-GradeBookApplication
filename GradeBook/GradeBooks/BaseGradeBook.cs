11633560b92ddd71410a76af8772f8ba93c0a879
ChumachenkoMykola <mykola.chumachenko@microsoft.wsei.edu.pl>
2025-03-13 10:04:25 +0100

    19/5

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,15 +9,19 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
+        public GradeBookType Enum { get; set; }
+        public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -106,20 +110,30 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid letter date");
             }
-            return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                gpa += 1;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
@@ -247,7 +261,7 @@ namespace GradeBook.GradeBooks
                 if (string.IsNullOrEmpty(gradeBookType))
                     gradeBookType = "Standard";
                 else
-                    gradeBookType = Enum.GetName(gradebookEnum, int.Parse(gradeBookType));
+                    gradeBookType = Enums.GradeBookType.GetName(gradebookEnum, int.Parse(gradeBookType));
             }
 
             // Get GradeBook from the GradeBook.GradeBooks namespace
---
+++ GradeBook/GradeBooks/GradeBooks.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+            var sortedGrades = Students.Select(s => s.AverageGrade).OrderByDescending(g => g).ToList();
+            int threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            if (averageGrade >= sortedGrades[threshold - 1])
+                return 'A';
+            if (averageGrade >= sortedGrades[threshold *2 - 1])
+                return 'B';
+            if (averageGrade >= sortedGrades[threshold *3 - 1])
+                return 'C';
+            if (averageGrade >= sortedGrades[threshold * 4 - 1])
+                return 'D';
+            return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            else base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            else base.CalculateStudentStatistics(name);
+        }
+
+    }
+}
